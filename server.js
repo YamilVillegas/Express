4a5fad816b84eaea0568b444cef1c9b1121a2670
Yamil Villegas <YTorres87@gmail.com>
2022-01-24 20:29:55 -0500

    More Routes Week One Assignment

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 // states that we are using express & morgan in this file
 const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
 // Brings in campsiteRouter.js
+const promotionRouter = require('./routes/promotionRouter')
 
 const hostname = 'localhost';
 const port = 3000;
@@ -15,34 +17,9 @@ app.use(express.json());
 // Express Middleware function
 app.use('/campsites', campsiteRouter);
 // Brings in campsiteRouter
-
-app.all('/campsites',(req,res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
-// We are sending plain text as a response. The next() function is passing control of the routing to the next relevant routing method after this one. Otherwise it will stop here and not go any further.
-
-app.get('/campsites', (req, res) => {
-    res.end('Will send all campsites to you');
-});
-// This ends the response and sends the plain text string back to the client.
-
-app.post('/campsites', (req,res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-// If the client uses a POST method it will jump from app.all to app.post, skipping the app.get method since it is not a GET request. The EXPRESS.JSON takes the properties from the JSON data that it receives and sets it up as properties of the req.body javascript object. 
-
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
-// We are rejecting the request if the client requests a PUT.
-
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
-// We will be adding authentication priviliges to this later on.
+app.use('/promotions', promotionRouter);
+// Brings in promotionRouter
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 // Directs express to serve static files from the public folder
